dea7b7323f17214792e1e3a1dbb32370964f3570
huang-jeff <jeffrey.huang.ece@gmail.com>
2018-09-21 11:42:56 -0400

    add additional functionality

--- src/main/java/com/example/demo/rabbitmq/Sender.java
+++ src/main/java/com/example/demo/rabbitmq/Sender.java
@@ -5,21 +5,22 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 public class Sender {
-	
+
 	private final static String QUEUE_NAME = "hello";
 	
 	public static void send() throws Exception {
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
-		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
-	
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-		String message = "Hello World!";
-		channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-		System.out.println(" [x] Sent '" + message + "'");
-		
-		channel.close();
-		connection.close();
+		try (
+			Connection connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+		) {
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			String message = "Hello World!";
+			channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+			System.out.println(" [x] Sent '" + message + "'");
+		} catch (Exception e) {
+			// do nothing
+		}
 	}
 }
